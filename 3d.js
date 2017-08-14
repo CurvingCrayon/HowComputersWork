dfc3ec24c0fd048bad668360ffc21e45789aa463
CurvingCrayon <nin10dog@gmail.com>
2017-08-15 07:01:20 +1000

    Removed files

--- 3d.js
+++ 3d.js
@@ -118,6 +118,18 @@ var scene, camera, renderer;
 			renderer.render( scene, camera );
 
 		}
+		function center(mesh){
+			    var middle = new THREE.Vector3();
+				var geometry = mesh.geometry;
+
+				geometry.computeBoundingBox();
+
+				middle.x = (geometry.boundingBox.max.x + geometry.boundingBox.min.x) / 2;
+				middle.y = (geometry.boundingBox.max.y + geometry.boundingBox.min.y) / 2;
+				middle.z = (geometry.boundingBox.max.z + geometry.boundingBox.min.z) / 2;
+
+				mesh.localToWorld( middle );
+		}
 
 		window.addEventListener( 'mousemove', onMouseMove, false );
 		window.addEventListener( 'touchmove', onMouseMove, false );
